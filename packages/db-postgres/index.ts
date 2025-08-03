48035a9c3f2847d6c380278008987eca9696085e
Anthony Bouch <anthony@infonomic.io>
2025-08-03 07:01:50 +0700

    feat: placeholder db init function for byline.config.ts

---
+++ packages/db-postgres/index.ts
@@ -0,0 +1,4 @@
+// TODO: implement db adapter initializing function to be 
+// based to byline.config.ts
+export * from './src/storage/storage-commands';
+export * from './src/storage/storage-queries';
